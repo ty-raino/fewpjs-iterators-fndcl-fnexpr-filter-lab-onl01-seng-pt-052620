cbb10e9e1505740fdeefae5f2e23204d598c3389
github username <github email address>
2020-12-16 00:04:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(driverArr, name) {
+   return driverArr.filter( n => {
+        return n.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(driverArr, search) {
+    return driverArr.filter( n =>{
+        return n.toLowerCase().startsWith(search.toLowerCase());
+    })
+}
+
+function matchName(driverArr, search) {
+    return driverArr.filter( driver => {
+        return driver.name.toLowerCase() === search.toLowerCase();
+    })
+}
